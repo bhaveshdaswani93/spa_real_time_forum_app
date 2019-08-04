b622376460ba733843087af79dd47fde92648838
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2019-08-04 09:18:21 +0530

    Factory and seed

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,9 +4,12 @@
 
 use App\Model\Category;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(Category::class, function (Faker $faker) {
+    $category = $faker->word;
     return [
-        //
+        'name' => $category,
+        'slug' => Str::slug($category)
     ];
 });
--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -4,9 +4,18 @@
 
 use App\Model\Reply;
 use Faker\Generator as Faker;
+use App\User;
+use App\Model\Question;
 
 $factory->define(Reply::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->text(),
+        'user_id' => function() {
+            return User::all()->random();
+        },
+        'question_id' => function() {
+            return Question::all()->random();
+        }
+
     ];
 });
